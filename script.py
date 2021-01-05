50dfda06e214cad07181f17b9a8e349c3aae3292
Arthur <arthur.bosshardt@gmail.com>
2021-01-05 17:43:03 +0100

    Ajout de la prise en compte de la feuille SI-erreur

--- script.py
+++ script.py
@@ -4,7 +4,29 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-data_climat = pd.read_excel('./data/Climat.xlsx', header=2, usecols="D:O").iloc[1:32]
+def update_annotation(ind):
+    x,y = line.get_data()
+    annotation.xy = (x[ind["ind"][0]], y[ind["ind"][0]])
+    text = year_values[ind["ind"][0]]
+    annotation.set_text(text)
+    annotation.get_bbox_patch().set_alpha(0.4)
+
+def hover(event):
+    vis = annotation.get_visible()
+    if event.inaxes == ax:
+        cont, ind = line.contains(event)
+        if cont:
+            update_annotation(ind)
+            annotation.set_visible(True)
+            fig.canvas.draw_idle()
+        else:
+            if vis:
+                annotation.set_visible(False)
+                fig.canvas.draw_idle()
+
+# ------------ Traitement de la feuille SI ------------
+
+data_climat = pd.read_excel('./data/Climat.xlsx', 'SI ', header=2, usecols="D:O").iloc[1:32]
 df = pd.DataFrame(data_climat)
 
 # Calcul des moyennes par mois
@@ -38,29 +60,48 @@ ax.set(xlabel='Jour de l\'année', ylabel='Temperature (°C)', title='Evolution 
 annotation = ax.annotate("", xy=(0,0), xytext=(-20,20),textcoords="offset points", bbox=dict(boxstyle="round", fc="w"), arrowprops=dict(arrowstyle="->"))
 annotation.set_visible(False)
 
-def update_annotation(ind):
-    x,y = line.get_data()
-    annotation.xy = (x[ind["ind"][0]], y[ind["ind"][0]])
-    text = year_values[ind["ind"][0]]
-    annotation.set_text(text)
-    annotation.get_bbox_patch().set_alpha(0.4)
-
-def hover(event):
-    vis = annotation.get_visible()
-    if event.inaxes == ax:
-        cont, ind = line.contains(event)
-        if cont:
-            update_annotation(ind)
-            annotation.set_visible(True)
-            fig.canvas.draw_idle()
-        else:
-            if vis:
-                annotation.set_visible(False)
-                fig.canvas.draw_idle()
-
 fig.canvas.mpl_connect("motion_notify_event", hover)
 
 plt.show(block=False)
 plt.show()
 
+# --------- Traitement de la feuille SI-erreur ---------
 
+data_climat = pd.read_excel('./data/Climat.xlsx', 'SI -erreur', header=2, usecols="D:O").iloc[1:32]
+df = pd.DataFrame(data_climat)
+
+# Calcul des moyennes par mois
+print(np.mean(df))
+
+# Ecart-type par mois
+print(np.std(df))
+
+# Minimum par mois
+print(df.min())
+
+# Maximum par mois
+print(df.max())
+
+# Affichage des vues mensuelles
+month_number = 0
+for month in data_climat.head():
+    plt.xlabel('Jours de '+month)
+    plt.ylabel('Température (°C)')
+    plt.plot(df.iloc[:,month_number])
+    month_number = month_number+1
+    plt.show()
+
+# Affichage de la vue annuelle 
+fig, ax = plt.subplots()
+
+year_values = np.concatenate((df.iloc[:, 0], df.iloc[:, 1], df.iloc[:, 2], df.iloc[:, 3], df.iloc[:, 4], df.iloc[:, 5], df.iloc[:, 6], df.iloc[:, 7], df.iloc[:, 8], df.iloc[:, 9], df.iloc[:, 10], df.iloc[:, 11]))
+year_values = [x for x in year_values if str(x) != 'nan']
+line, = ax.plot(year_values)
+ax.set(xlabel='Jour de l\'année', ylabel='Temperature (°C)', title='Evolution de la température sur l\'année')
+annotation = ax.annotate("", xy=(0,0), xytext=(-20,20),textcoords="offset points", bbox=dict(boxstyle="round", fc="w"), arrowprops=dict(arrowstyle="->"))
+annotation.set_visible(False)
+
+fig.canvas.mpl_connect("motion_notify_event", hover)
+
+plt.show(block=False)
+plt.show()
