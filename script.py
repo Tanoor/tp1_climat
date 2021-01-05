a933d0cf9067c2265ec545fbc50c6c6ca5d67c41
Arthur <arthur.bosshardt@gmail.com>
2021-01-05 16:31:19 +0100

    Lecture du fichier climat

--- script.py
+++ script.py
@@ -3,3 +3,6 @@ from scipy import misc
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
+data_climat = pd.read_excel('./data/Climat.xlsx', header=2, usecols="D:O").iloc[1:32]
+df = pd.DataFrame(data_climat)
