732cbbddd10a6d8a9796decd18ff25af3801c7a4
Arthur <arthur.bosshardt@gmail.com>
2021-01-05 17:27:24 +0100

    Ajout du calcul moyenne, écart-type, min et max par mois

--- script.py
+++ script.py
@@ -6,3 +6,15 @@ import numpy as np
 
 data_climat = pd.read_excel('./data/Climat.xlsx', header=2, usecols="D:O").iloc[1:32]
 df = pd.DataFrame(data_climat)
+
+# Calcul des moyennes par mois
+print(np.mean(df))
+
+# Ecart-type par mois
+print(np.std(df))
+
+# Minimum par mois
+print(df.min())
+
+# Maximum par mois
+print(df.max())
